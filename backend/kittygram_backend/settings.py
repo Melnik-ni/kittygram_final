f24c0f8a8f21295308f69a688e0e87958a81f35b
Николай Ибраев <me1nik.nik@yandex.ru>
2023-06-25 11:56:15 +0300

    Комент

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['hkotiki.serveftp.com']
+ALLOWED_HOSTS = ['158.160.28.251', '127.0.0.1', 'localhost', 'kotiki.serveftp.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
